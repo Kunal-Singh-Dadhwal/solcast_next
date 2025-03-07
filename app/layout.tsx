616e0097cfbf68b9b98147107e2fd8679eaf1a69
Pranjal1804 <mitrapranjal2004@gmailcom>
2025-03-07 20:27:08 +0530

    styling changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Solcast',
+  description: 'Created by Solcast',
+  generator: 'Solcast',
 }
 
 export default function RootLayout({
